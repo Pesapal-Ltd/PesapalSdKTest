6525b14b68a687cd2223e0d4e6b4d4bc70024fe0
Richard <richard@pesapal.com>
2024-03-18 15:20:11 +0300

    Initial Commit

---
+++ Sources/PesapalSdK/PesapalSdK.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public func functionA(appStr: String) -> String{
+    return appStr
+}
+
+public func callView() -> any View{
+    return SwiftUIView().navigationBarHidden(true)
+}
+
---
+++ Sources/PesapalSdK/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Richard kamere on 3/18/24.
+//
+
+import SwiftUI
+
+
+public struct SwiftUIView: View {
+    public init() {}
+    public var body: some View {
+        Text("Hello, am pesapal sdk ")
+    }
+}
+
+//#Preview {
+//    SwiftUIView()
+//}
